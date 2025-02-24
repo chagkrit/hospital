2d0d45f096f07db6699baefb522beb3e50575a98
chagkrit <110458569+chagkrit@users.noreply.github.com>
2025-02-24 22:11:27 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const coordinates = {
+        "SkyWalk": { coords: [2, 2], image: "images/path/SkyWalk.jpg", description: "จุดเริ่มต้น Sky Walk" },
+        "SriPathGate": { coords: [3, 3], image: "images/path/SriPathGate.jpg", description: "ประตูศรีพัฒน์" },
+        "OPD9": { coords: [5, 5], image: "images/path/OPD9.jpg", description: "ห้องตรวจ OPD 9" },
+        "OPD101": { coords: [8, 4], image: "images/path/OPD101.jpg", description: "ห้องตรวจ OPD 101" },
+        "OPD2": { coords: [6, 6], image: "images/path/OPD2.jpg", description: "ห้องตรวจ OPD 2" },
+        "OPD110": { coords: [7, 7], image: "images/path/OPD110.jpg", description: "ห้องตรวจ OPD 110" },
+        "BloodRoom": { coords: [4, 9], image: "images/path/BloodRoom.jpg", description: "ห้องเจาะเลือด" },
+        "XRay33": { coords: [9, 3], image: "images/path/XRay33.jpg", description: "ห้อง X-Ray เบอร์ 33" },
+        "XRay34": { coords: [10, 5], image: "images/path/XRay34.jpg", description: "ห้อง X-Ray เบอร์ 34" }
+    };
